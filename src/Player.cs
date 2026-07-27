1c8ec31760c7a954b82fc63d2a00667a5ba8990e
mojofawad <mojofawad@gmail.com>
2026-07-26 20:55:34 -0400

    add player paddle with movement

---
+++ src/Player.cs
@@ -0,0 +1,44 @@
+using System;
+using Godot;
+
+namespace pong;
+
+public partial class Player : Area2D
+{
+	[Export]
+	public int Speed { get; set; } = 400;
+
+	public Vector2 ScreenSize;
+	
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
+	}
+	
+	public override void _Process(double delta)
+	{
+		var velocity = Vector2.Zero;
+		
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
+
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
+
+		if (velocity.Length() > 0)
+		{
+			velocity *= Speed;
+		}
+
+
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X), // this is unnecessary, but I'm not sure what I need
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y) 
+		);
+	}
+}
