2bd620f2d16629911cc1626735899f166efdf8ac
Joey Fawad <54484850+mojofawad@users.noreply.github.com>
2026-07-27 10:56:18 -0400

    feat: add player paddle with movement (#1)
    * add player paddle with movement
    * create initial 1:1 aspect ratio
    * lock player paddle in x direction

---
+++ src/Player.cs
@@ -0,0 +1,42 @@
+using Godot;
+
+namespace Pong;
+
+public partial class Player : Area2D
+{
+	[Export]
+	public int Speed { get; set; } = 400;
+
+	public Vector2 ScreenSize;
+	
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
+	}
+	
+	public override void _Process(double delta)
+	{
+		var velocity = Vector2.Zero;
+		
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
+
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
+
+		if (velocity.Length() > 0)
+		{
+			velocity *= Speed;
+		}
+		
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, 0),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y) 
+		);
+	}
+}
